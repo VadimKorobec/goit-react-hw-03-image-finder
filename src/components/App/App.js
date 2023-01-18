826b95a824169504e1da419f31e8bd6d149a0afb
VadimKorobec <_s_e_t__@ukr.net>
2023-01-18 15:15:00 +0200

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,3 +1,22 @@
-export const App = () => {
-  return <div></div>;
-};
+import { Modal } from 'components/Modal/Modal';
+import { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    showModal: false,
+  };
+
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({
+      showModal: !showModal,
+    }));
+  };
+
+  render() {
+    return (
+      <div>
+        <Modal />
+      </div>
+    );
+  }
+}
