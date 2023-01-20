e8d2d5348771cca5227818f40d28eb5ad39012c5
VadimKorobec <_s_e_t__@ukr.net>
2023-01-20 14:14:31 +0200

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,12 @@
+import { Button } from 'components/Button/Button';
+import { ImageGallery } from 'components/ImageGallery/ImageGallery';
 import { Modal } from 'components/Modal/Modal';
 import { Searchbar } from 'components/Searchbar/Searchbar';
 import { Component } from 'react';
 
 export class App extends Component {
   state = {
-    images: [],
+    images: null,
     showModal: false,
   };
 
@@ -13,7 +15,7 @@ export class App extends Component {
       'https://pixabay.com/api/?q=cat&page=1&key=31618598-dd0b87f36bc5180b6dfd99237&image_type=photo&orientation=horizontal&per_page=12'
     )
       .then(res => res.json())
-      .then(console.log);
+      .then(images => this.setState);
   }
 
   toggleModal = () => {
@@ -28,6 +30,8 @@ export class App extends Component {
     return (
       <div>
         <Searchbar />
+        <ImageGallery />
+        <Button />
 
         {showModal && (
           <Modal onClose={this.toggleModal}>
