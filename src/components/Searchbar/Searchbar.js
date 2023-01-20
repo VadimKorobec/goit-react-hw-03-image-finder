f356f66e3a9ca5726e8599232d8061c1ef05768e
VadimKorobec <_s_e_t__@ukr.net>
2023-01-20 16:23:48 +0200

    Update Searchbar.js

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import { toast } from 'react-toastify';
 
 export class Searchbar extends Component {
   state = {
@@ -6,19 +7,34 @@ export class Searchbar extends Component {
   };
 
   handleChange = event => {
-    this.setState({ query: event.currentTarget.value });
+    this.setState({ query: event.currentTarget.value.toLowerCase() });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    if (this.state.query.trim() === '') {
+      toast.error('Please specify your query!');
+      return;
+    }
+    this.props.onSubmit(this.state.query);
+
+    this.setState({ query: '' });
   };
 
   render() {
+    const { query } = this.state;
+
     return (
       <header>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <button type="submit">
             <span>Search</span>
           </button>
 
           <input
             type="text"
+            name="query"
+            value={query}
             autoComplete="off"
             autoFocus
             placeholder="Search images and photos"
