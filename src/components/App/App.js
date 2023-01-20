575dadfd9856f01ed45a97da33f3ff5cf0f70bd7
VadimKorobec <_s_e_t__@ukr.net>
2023-01-20 13:37:36 +0200

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,8 +3,17 @@ import { Component } from 'react';
 
 export class App extends Component {
   state = {
+    images: [],
     showModal: false,
   };
+
+  componentDidMount() {
+    fetch(
+      'https://pixabay.com/api/?q=cat&page=1&key=31618598-dd0b87f36bc5180b6dfd99237&image_type=photo&orientation=horizontal&per_page=12'
+    )
+      .then(res => res.json())
+      .then(console.log);
+  }
 
   toggleModal = () => {
     this.setState(({ showModal }) => ({
