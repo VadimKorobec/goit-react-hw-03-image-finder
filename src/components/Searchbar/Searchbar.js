1a858759b334e75ef42b0a6bd56c80cac92ad094
VadimKorobec <_s_e_t__@ukr.net>
2023-01-20 14:42:24 +0200

    Update Searchbar.js

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,18 +1,31 @@
-export const Searchbar = () => {
-  return (
-    <header>
-      <form>
-        <button type="submit">
-          <span>Search</span>
-        </button>
+import { Component } from 'react';
 
-        <input
-          type="text"
-          autoComplete="off"
-          autoFocus
-          placeholder="Search images and photos"
-        />
-      </form>
-    </header>
-  );
-};
+export class Searchbar extends Component {
+  state = {
+    query: '',
+  };
+
+  handleChange = event => {
+    this.setState({ query: event.currentTarget.value });
+  };
+
+  render() {
+    return (
+      <header>
+        <form>
+          <button type="submit">
+            <span>Search</span>
+          </button>
+
+          <input
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            onChange={this.handleChange}
+          />
+        </form>
+      </header>
+    );
+  }
+}
