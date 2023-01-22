794197cbeb4fc94de1dec515070b6ad7541ab3a5
VadimKorobec <_s_e_t__@ukr.net>
2023-01-22 14:45:33 +0200

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,22 @@
 import { Searchbar } from 'components/Searchbar/Searchbar';
 import { Component } from 'react';
+import { fetchImages } from 'services/pixabayApi';
 
 export class App extends Component {
   state = {
     query: '',
+    page: 1,
     images: [],
   };
+
+  componentDidUpdate(_, prevState) {
+    const { query, page } = this.state;
+    if (prevState.query !== query || prevState.page !== page) {
+      fetchImages(query, page).then(resp => {
+        this.setState(prev => ({ images: [...prev.images, ...resp.hits] }));
+      });
+    }
+  }
 
   handleSubmit = query => {
     this.setState({ query });
