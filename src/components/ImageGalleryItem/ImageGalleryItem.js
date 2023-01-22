a651d9fb5d88a1403effedbb5fe5657a0f268014
VadimKorobec <_s_e_t__@ukr.net>
2023-01-22 18:51:38 +0200

    Update ImageGalleryItem.js

--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -6,12 +6,27 @@ export class ImageGalleryItem extends Component {
     isModalOpen: false,
   };
 
+  componentDidMount() {
+    window.addEventListener('keydown', this.onEscapePress);
+  }
+
+  componentWillUnmount() {
+    window.addEventListener('keydown', this.onEscapePress);
+  }
+
+  onEscapePress = event => {
+    if (event.code === 'Escape') {
+      this.props.onClouse();
+    }
+  };
+
   handleToggelModal = () => {
     this.setState(prevState => ({ isModalOpen: !prevState.isModalOpen }));
   };
 
   render() {
     const { webformatURL, tags, largeImageURL } = this.props.image;
+    const { isModalOpen } = this.state;
     return (
       <>
         <li className="ImageGalleryItem">
@@ -22,7 +37,9 @@ export class ImageGalleryItem extends Component {
             onClick={this.handleToggelModal}
           />
         </li>
-        <Modal url={largeImageURL} onClouse={this.handleToggelModal} />
+        {isModalOpen && (
+          <Modal url={largeImageURL} onClouse={this.handleToggelModal} />
+        )}
       </>
     );
   }
