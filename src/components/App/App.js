3b3b8db42fde2cc9ab020b225381db23d3256355
VadimKorobec <_s_e_t__@ukr.net>
2023-01-22 13:43:29 +0200

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,21 @@
+import { Searchbar } from 'components/Searchbar/Searchbar';
 import { Component } from 'react';
 
 export class App extends Component {
+  state = {
+    query: '',
+    images: [],
+  };
+
+  handleSubmit = query => {
+    this.setState({ query });
+  };
+
   render() {
-    return;
+    return (
+      <>
+        <Searchbar onSubmit={this.handleSubmit} />
+      </>
+    );
   }
 }
