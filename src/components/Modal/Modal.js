43ca72ae21d756c77881c18417c43ede764e8de1
VadimKorobec <_s_e_t__@ukr.net>
2023-01-22 13:04:16 +0200

    Update Modal.js

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,36 +1,3 @@
-import { Component } from 'react';
-import { createPortal } from 'react-dom';
-import { ModalStyled, Overlay } from './Modal.styled';
-
-const modalRoot = document.querySelector('#modal-root');
-
-export class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
-
-  handleKeyDown = event => {
-    if (event.code === 'Escape') {
-      this.props.onClose();
-    }
-  };
-
-  handleBackdropClick = event => {
-    if (event.currentTarget === event.target) {
-      this.props.onClose();
-    }
-  };
-
-  render() {
-    return createPortal(
-      <Overlay onClick={this.handleBackdropClick}>
-        <ModalStyled>{this.props.children}</ModalStyled>
-      </Overlay>,
-      modalRoot
-    );
-  }
-}
+export const Modal = () => {
+  return;
+};
