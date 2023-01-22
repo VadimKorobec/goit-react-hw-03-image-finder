2147d8466c5754ce30f55d5d830f5cef2c236e37
VadimKorobec <_s_e_t__@ukr.net>
2023-01-22 18:15:26 +0200

    Update Modal.js

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,3 +1,13 @@
-export const Modal = () => {
-  return;
-};
+import { Component } from 'react';
+
+export class Modal extends Component {
+  render() {
+    return (
+      <div class="overlay">
+        <div class="modal">
+          <img src="" alt="" />
+        </div>
+      </div>
+    );
+  }
+}
