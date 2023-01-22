96922944b3df3e290aceaac19b1a8c8afae66ce5
VadimKorobec <_s_e_t__@ukr.net>
2023-01-22 18:15:27 +0200

    Update ImageGalleryItem.js

--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -1,11 +1,29 @@
-export const ImageGalleryItem = ({ image }) => {
-  return (
-    <li className="ImageGalleryItem">
-      <img
-        className="ImageGalleryItem-image"
-        src={image.webformatURL}
-        alt={image.tags}
-      />
-    </li>
-  );
-};
+import { Modal } from 'components/Modal/Modal';
+import { Component } from 'react';
+
+export class ImageGalleryItem extends Component {
+  state = {
+    isModalOpen: false,
+  };
+
+  handleToggelModal = () => {
+    this.setState(prevState => ({ isModalOpen: !prevState.isModalOpen }));
+  };
+
+  render() {
+    const { webformatURL, tags, largeImageURL } = this.props.image;
+    return (
+      <>
+        <li className="ImageGalleryItem">
+          <img
+            className="ImageGalleryItem-image"
+            src={webformatURL}
+            alt={tags}
+            onClick={this.handleToggelModal}
+          />
+        </li>
+        <Modal url={largeImageURL} onClouse={this.handleToggelModal} />
+      </>
+    );
+  }
+}
