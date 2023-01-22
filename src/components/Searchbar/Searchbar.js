f95c4a5d0d731ac1970831121d79a62ba2c0c4ff
VadimKorobec <_s_e_t__@ukr.net>
2023-01-22 13:43:27 +0200

    Update Searchbar.js

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,3 +1,23 @@
-export const Searchbar = () => {
-  return;
+export const Searchbar = ({ onSubmit }) => {
+  const handleSubmitForm = event => {
+    event.preventDefault();
+    onSubmit(event.currentTarget.query.value);
+  };
+  return (
+    <header>
+      <form onSubmit={handleSubmitForm}>
+        <button type="submit">
+          <span>Search</span>
+        </button>
+
+        <input
+          type="text"
+          name="query"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+        />
+      </form>
+    </header>
+  );
 };
