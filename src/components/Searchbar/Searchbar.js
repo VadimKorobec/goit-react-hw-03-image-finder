5a34cc8dda3e8b43186b403c67fff1b4a5119b66
VadimKorobec <_s_e_t__@ukr.net>
2023-01-23 12:30:05 +0200

    Update Searchbar.js

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,3 +1,6 @@
+import PropTypes from 'prop-types';
+import { Search } from './Searchbar.styled';
+
 export const Searchbar = ({ onSubmit }) => {
   const handleSubmitForm = event => {
     event.preventDefault();
@@ -10,7 +13,7 @@ export const Searchbar = ({ onSubmit }) => {
         <button type="submit" className="SearchForm-button">
           <span className="SearchForm-button-label">Search</span>
         </button>
-
+        <Search />
         <input
           className="SearchForm-input"
           type="text"
@@ -22,4 +25,8 @@ export const Searchbar = ({ onSubmit }) => {
       </form>
     </header>
   );
+};
+
+Searchbar.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
 };
