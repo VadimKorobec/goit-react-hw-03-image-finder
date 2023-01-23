bfaa93be4a105afcead8779f9e9354cdc09f7547
VadimKorobec <_s_e_t__@ukr.net>
2023-01-23 12:46:32 +0200

    Update Modal.js

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -2,10 +2,24 @@ import PropTypes from 'prop-types';
 import { Component } from 'react';
 
 export class Modal extends Component {
+  componentDidMount() {
+    window.addEventListener('keydown', this.onEscapePress);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.onEscapePress);
+  }
+
+  onEscapePress = evt => {
+    if (evt.code === 'Escape') {
+      this.props.onClose();
+    }
+  };
+
   render() {
-    const { url, onClouse } = this.props;
+    const { url, onClose } = this.props;
     return (
-      <div className="Overlay" onClick={onClouse}>
+      <div className="Overlay" onClick={onClose}>
         <div className="Modal">
           <img src={url} alt="IMG" />
         </div>
